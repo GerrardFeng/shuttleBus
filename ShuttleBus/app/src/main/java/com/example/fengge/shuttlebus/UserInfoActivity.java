75a7c33c13ea39d4f9761c73d47ca2607d26dc4f
wury <ryan.wu@oocl.com>
2016-01-12 17:19:06 +0800

    modify usertype information

--- ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/UserInfoActivity.java
+++ ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/UserInfoActivity.java
@@ -1,16 +1,21 @@
 package com.example.fengge.shuttlebus;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by WURY on 1/12/2016.
  */
 public class UserInfoActivity extends Activity {
 
+    private int selectedUserType = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +27,36 @@ public class UserInfoActivity extends Activity {
 
         userTypeButton.setOnClickListener(new userTypeButtonListener());
         showTickets.setOnClickListener(new showTicketsButtonListener());
-
     }
 
     class userTypeButtonListener implements View.OnClickListener{
 
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(UserInfoActivity.this, MainActivity.class);
-            startActivity(intent);
+            final String[] arrayUserType = new String[] { "普通上下班", "轮班"};
+            Dialog alertDialog = new AlertDialog.Builder(UserInfoActivity.this).
+                    setTitle("修改用户类型")
+                    .setSingleChoiceItems(arrayUserType, 0, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            selectedUserType = which;
+                        }
+                    }).
+                    setPositiveButton("确认", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(UserInfoActivity.this, arrayUserType[selectedUserType], Toast.LENGTH_SHORT).show();
+                        }
+                    }).
+                    setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // TODO Auto-generated method stub
+                        }
+                    }).
+                    create();
+            alertDialog.show();
         }
     }
 
