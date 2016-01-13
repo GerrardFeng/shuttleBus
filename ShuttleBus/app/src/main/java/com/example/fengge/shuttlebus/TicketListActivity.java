3734894461c84530d7406b0532a525f48f739c64
WEICA <WEICA@OOCL.COM>
2016-01-13 11:06:34 +0800

    Ticket list view activity and layout.
    Signed-off-by: WEICA <WEICA@OOCL.COM>

--- ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/TicketListActivity.java
+++ ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/TicketListActivity.java
@@ -1,15 +1,20 @@
 package com.example.fengge.shuttlebus;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -35,18 +40,25 @@ public class TicketListActivity extends Activity {
 
     private List<HashMap<String, String>> initTicketData() {
         List<HashMap<String, String>> datas = new ArrayList<HashMap<String, String>>();
-        HashMap<String, String> item = new HashMap<String, String>();
-        item.put("ticketInfo", "姓名：test 线路： test 站点： test 类型： test");
-        datas.add(item);
+        for(int i=0; i<5; i++) {
+            HashMap<String, String> item = new HashMap<String, String>();
+            item.put("ticketInfo", "姓名：test" + i + " 线路： test 站点： test 类型： test");
+            datas.add(item);
+        }
         return datas;
     }
 
 
-    private void initListViewEvent(List<HashMap<String, String>> routData) {
-        ListView listView = (ListView) this.findViewById(R.id.ticketListView);
-        SimpleAdapter adapter = new SimpleAdapter(this, routData, R.layout.ticket_item, new String[] { "ticketInfo"},
-                new int[] { R.id.ticketInfo });
-        listView.setAdapter(adapter);
+    private void initListViewEvent(List<HashMap<String, String>> ticketData) {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ticketListLayout);
+
+        for(HashMap<String, String> ticket : ticketData) {
+            TextView textView1 = new TextView(this);
+            textView1.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            textView1.setText(ticket.get("ticketInfo"));
+            linearLayout.addView(textView1);
+        }
     }
 
 
