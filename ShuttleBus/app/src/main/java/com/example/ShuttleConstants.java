a95c969252ccf8fd6515a8b33acad54abe06e13b
FENGGE <gerrard.feng@oocl.com>
2016-01-13 16:54:13 +0800

    getTicket integration with back-end

--- ShuttleBus/app/src/main/java/com/example/ShuttleConstants.java
+++ ShuttleBus/app/src/main/java/com/example/ShuttleConstants.java
@@ -15,4 +15,7 @@ public class ShuttleConstants {
 
     public static String LOGIN_SUCCESS = "success";
 
+    public static String USER_ID = "userid";
+
+    public static String SHOW_TICKET = "SHOW_TICKET";
 }
