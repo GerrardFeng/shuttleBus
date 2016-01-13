5e9dac656fc68a7309aa74e66516f63a2c6a47a7
wury <ryan.wu@oocl.com>
2016-01-13 11:23:52 +0800

    search

--- ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/SearchActivity.java
+++ ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/SearchActivity.java
@@ -1,23 +1,20 @@
 package com.example.fengge.shuttlebus;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import java.util.Calendar;
 
 public class SearchActivity extends Activity {
 
-    private ListView listView;
-    List<String> data ;
-//    private TimelineAdapter timelineAdapter;
+    private Calendar calendar;
+    private int year, month, day;
+    private EditText dateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +22,39 @@ public class SearchActivity extends Activity {
         setContentView(R.layout.activity_search);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-//        listView = (ListView) this.findViewById(R.id.listview);
-//        listView.setDividerHeight(0);
-//        timelineAdapter = new TimelineAdapter(this, getData());
-//        listView.setAdapter(timelineAdapter);
+        dateText = (EditText)findViewById(R.id.search_key);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month + 1, day);
     }
 
-//    private List<Map<String, Object>> getData() {
-//        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-//
-//        Map<String, Object> map = new HashMap<String, Object>();
-//        map.put("title", "这是第1行测试数据");
-//        list.add(map);
-//
-//        map = new HashMap<String, Object>();
-//        map.put("title", "这是第2行测试数据");
-//        list.add(map);
-//
-//        map = new HashMap<String, Object>();
-//        map.put("title", "这是第3行测试数据");
-//        list.add(map);
-//
-//        map = new HashMap<String, Object>();
-//        map.put("title", "这是第4行测试数据");
-//        list.add(map);
-//        return list;
-//    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            showDate(arg1, arg2+1, arg3);
+
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        dateText.setText(new StringBuilder().append(month).append("/").append(day).append("/").append(year));
+    }
 
 }
