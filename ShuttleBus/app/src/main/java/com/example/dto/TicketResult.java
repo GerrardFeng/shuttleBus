3068a2f3e0aacc3f1c85562572aa23ee667b33d0
FENGGE <gerrard.feng@oocl.com>
2016-01-13 16:14:13 +0800

    TicketResult Dto

---
+++ ShuttleBus/app/src/main/java/com/example/dto/TicketResult.java
@@ -0,0 +1,35 @@
+package com.example.dto;
+
+public class TicketResult {
+	
+	private String username;
+	private String route;
+	private String station;
+	private String type;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getRoute() {
+		return route;
+	}
+	public void setRoute(String route) {
+		this.route = route;
+	}
+	public String getStation() {
+		return station;
+	}
+	public void setStation(String station) {
+		this.station = station;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
