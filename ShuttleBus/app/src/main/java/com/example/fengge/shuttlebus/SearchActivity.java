06a1af9de0e233bdf01e08c7e1b4ef5e54e585de
wury <ryan.wu@oocl.com>
2016-01-13 17:07:40 +0800

    search IIII

--- ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/SearchActivity.java
+++ ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/SearchActivity.java
@@ -2,20 +2,14 @@ package com.example.fengge.shuttlebus;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -37,8 +31,7 @@ public class SearchActivity extends Activity {
         year = calendar.get(Calendar.YEAR);
         month = calendar.get(Calendar.MONTH) + 1;
         day = calendar.get(Calendar.DAY_OF_MONTH);
-//        showDate(year, month + 1, day);
-        List<HashMap<String, Object>> dateData = initDateData();
+        List<HashMap<String, Object>> dateData = initDateData(year, month, day);
         initModifyDateListViewEvent(dateData);
 
         List<HashMap<String, Object>> searchData = initSearchData();
@@ -63,7 +56,7 @@ public class SearchActivity extends Activity {
         listView.setAdapter(adapter);
     }
 
-    private List<HashMap<String, Object>> initDateData() {
+    private List<HashMap<String, Object>> initDateData(int year, int month, int day) {
         List<HashMap<String, Object>> dataList = new ArrayList<HashMap<String, Object>>();
         HashMap<String, Object> item = new HashMap<String, Object>();
         item.put("dateIcon", R.drawable.date);
@@ -87,31 +80,18 @@ public class SearchActivity extends Activity {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             ListView listView = (ListView) parent;
             HashMap<String, Object> data = (HashMap<String, Object>) listView.getItemAtPosition(position);
-            setDate(view);
-        }
-    }
 
-    @SuppressWarnings("deprecation")
-    public void setDate(View view) {
-        showDialog(999);
-    }
+            new DatePickerDialog(SearchActivity.this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker dateView, int currentYear, int currentMonth, int currentDay) {
+                    List<HashMap<String, Object>> dateData = initDateData(currentYear, currentMonth + 1, currentDay);
+                    initModifyDateListViewEvent(dateData);
+                    // TODO refresh data
 
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        if (id == 999) {
-            return new DatePickerDialog(this, myDateListener, year, month, day);
-        }
-        return null;
-    }
+                }
+            },calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH) ).show();
 
-    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
-//            showDate(arg1, arg2 + 1, arg3);
-            List<HashMap<String, Object>> dateData = initDateData();
-            initModifyDateListViewEvent(dateData);
 
         }
-    };
-
+    }
 }
