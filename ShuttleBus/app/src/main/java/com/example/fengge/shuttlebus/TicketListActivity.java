7bea21be33f9d5c6eb92fc8e744296aabfb410f5
Luce Zhang <luce.zhang@oocl.com>
2016-01-14 20:49:44 +0800

    add refresh

--- ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/TicketListActivity.java
+++ ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/TicketListActivity.java
@@ -1,20 +1,38 @@
 package com.example.fengge.shuttlebus;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.ShuttleConstants;
 import com.example.dto.TicketResult;
 import com.example.jason.FastJasonTools;
+import com.joanzapata.iconify.IconDrawable;
+import com.joanzapata.iconify.Iconify;
+import com.joanzapata.iconify.fonts.EntypoModule;
+import com.joanzapata.iconify.fonts.FontAwesomeIcons;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
+import com.joanzapata.iconify.fonts.IoniconsModule;
+import com.joanzapata.iconify.fonts.MaterialCommunityModule;
+import com.joanzapata.iconify.fonts.MaterialModule;
+import com.joanzapata.iconify.fonts.MeteoconsModule;
+import com.joanzapata.iconify.fonts.SimpleLineIconsModule;
+import com.joanzapata.iconify.fonts.TypiconsModule;
+import com.joanzapata.iconify.fonts.WeathericonsModule;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.utils.CustomProgressDialog;
 import com.utils.HttpUtil;
 import com.utils.PropertiesUtil;
 import com.utils.SharePreferenceHelper;
@@ -41,8 +59,14 @@ public class TicketListActivity extends BaseActivity {
         String userId = SharePreferenceHelper.getUser(TicketListActivity.this).getId();
         Log.v("TicketListActivity",userId);
         Log.v("hustzw", "ONCLICK");
+        initIcon();
+        getUserTicketForUser(userId);
+        List<HashMap<String, String>> ticketData = initTicketData();
+    }
 
-        AsyncHttpClient client = new AsyncHttpClient();
+    private void getUserTicketForUser(String userId) {
+        final CustomProgressDialog progressLogin = new CustomProgressDialog(TicketListActivity.this);
+        progressLogin.show();
         RequestParams params = new RequestParams();
         params.put(ShuttleConstants.USER_ID, userId);
 
@@ -53,21 +77,41 @@ public class TicketListActivity extends BaseActivity {
                 Log.v("hustzw", "coming");
                 List<TicketResult> allTickets = FastJasonTools.getParseBeanArray(response.toString(), TicketResult.class);
                 initListViewEvent(allTickets);
+                progressLogin.hide();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
                 showTips(TicketListActivity.this, R.string.server_not_avaiable, false);
+                progressLogin.hide();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 super.onFailure(statusCode, headers, responseString, throwable);
+                showTips(TicketListActivity.this, R.string.server_not_avaiable, false);
+                progressLogin.hide();
             }
         });
+    }
 
-        List<HashMap<String, String>> ticketData = initTicketData();
+
+    private void initIcon() {
+        ImageButton but = (ImageButton)findViewById(R.id.button2);
+        but.setImageDrawable(new IconDrawable(this, FontAwesomeIcons.fa_refresh)
+                .colorRes(android.R.color.holo_blue_light)
+//                .color(android.R.color.white)//Color.parseColor("#ffffff")
+                .actionBarSize());
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId = SharePreferenceHelper.getUser(TicketListActivity.this).getId();
+                Log.v("Ticket with userId", userId);
+                getUserTicketForUser(userId);
+            }
+        });
+        but.invalidate();
     }
 
     private List<HashMap<String, String>> initTicketData() {
@@ -83,6 +127,8 @@ public class TicketListActivity extends BaseActivity {
 
     private void initListViewEvent(List<TicketResult> allTickets) {
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ticketListLayout);
+
+        linearLayout.removeAllViews();
 
         for(TicketResult ticket : allTickets) {
             TextView textView1 = new TextView(this);
