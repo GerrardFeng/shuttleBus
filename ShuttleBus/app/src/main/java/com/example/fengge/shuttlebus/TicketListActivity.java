7473f33e30df8e7eafd6b5bfbbe3674149e609c7
WEICA <WEICA@OOCL.COM>
2016-01-14 17:10:03 +0800

    UI style adjustment.
    Signed-off-by: WEICA <WEICA@OOCL.COM>

--- ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/TicketListActivity.java
+++ ShuttleBus/app/src/main/java/com/example/fengge/shuttlebus/TicketListActivity.java
@@ -86,9 +86,9 @@ public class TicketListActivity extends BaseActivity {
 
         for(TicketResult ticket : allTickets) {
             TextView textView1 = new TextView(this);
-            LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT);
-            llp.setMargins(0, 0, 0, 10);
+            llp.setMargins(5, 0, 5, 10);
             textView1.setLayoutParams(llp);
             textView1.setText("路线： " + ticket.getRoute() + "\n" +
                     "站点： " + ticket.getStation() + "\n" +
